b2e6f7322a5ec63e19aaff91b19ba5a7de477e22
Kirannagre <adinathnagre885@gmail.com>
2025-11-18 10:03:03 +0530

    style changed of paragraph

--- src/App.jsx
+++ src/App.jsx
@@ -9,11 +9,10 @@ function App() {
   return (
  <>
    <h1>Introduction</h1>
-   <p>React is a frontend Libraray used to design standalone single page application . <br />
-      React is a  library of javascript and it is completely based on reusable components . <br />
-      Why this help and provide reusble code because Each particular part of react should be written inside <br />
-      function or class so it can be resuse easily.  
-      Means We have to write only once can be call or used in multiple parts. <br />
+   <p style={{width:"45%", textAlign:'justify'}}>React is a frontend Libraray used to design standalone single page application . 
+      React is a  library of javascript and it is completely based on reusable components . 
+      Why this help and provide reusble code because Each particular part of react should be written inside
+      function or class so it can be resuse easily.  Means We have to write only once can be call or used in multiple parts. 
    </p>
  </>
   )
