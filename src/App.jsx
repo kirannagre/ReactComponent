dc7c05bd29d684f45085f375389368e48f8ec3f1
Kirannagre <adinathnagre885@gmail.com>
2025-11-18 09:54:50 +0530

    Changed in APP.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -7,28 +7,15 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+ <>
+   <h1>Introduction</h1>
+   <p>React is a frontend Libraray used to design standalone single page application . <br />
+      React is a  library of javascript and it is completely based on reusable components . <br />
+      Why this help and provide reusble code because Each particular part of react should be written inside <br />
+      function or class so it can be resuse easily.  
+      Means We have to write only once can be call or used in multiple parts. <br />
+   </p>
+ </>
   )
 }
 
