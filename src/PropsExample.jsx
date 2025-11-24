e6b2f72b36bd35880d952f74ed870266cf9e67ef
Kirannagre <adinathnagre885@gmail.com>
2025-11-24 09:50:41 +0530

    props example for properties in react

---
+++ src/PropsExample.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const PropsExample = (props) => {
+  return (
+    <div>
+      <h1>Hi, {props.name} || {props.role} </h1>
+    </div>
+  )
+}
+
+export default PropsExample
--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,12 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import Learn from './Learn.jsx'
+import PropsExample from './PropsExample.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Learn />
+    <PropsExample name="kiran" role="java developer"/>
+    <PropsExample name="niyaz" role="frontend developer"/>
+    <PropsExample name="Hariom" role="Commander"/>
   </StrictMode>,
 )
